647dbc5d15e789ef03d67efd6eabe3955111e9c1
Rocky Jay <rockyjay_g@yahoo.com>
2024-09-06 02:39:37 -0700

    redeploy

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -4,11 +4,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getDetailAnime } from "../services/api";
 import { ArticleDetail } from "./ArticleDetail";
 import { DetailLoading } from "./DetailLoading";
-import { useInView } from "react-intersection-observer";
 
 const Detail = () => {
   const { id } = useParams();
-  const { ref, inView } = useInView();
 
   const { data, isLoading } = useQuery({
     queryKey: ["detail-anime", id],
