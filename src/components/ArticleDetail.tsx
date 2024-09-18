3ac6c716217f6e814cad0b96a1dd93768cf869a7
Rocky Jay <rockyjay_g@yahoo.com>
2024-09-18 07:13:51 -0700

    redesign to make app faster

--- src/components/ArticleDetail.tsx
+++ src/components/ArticleDetail.tsx
@@ -5,7 +5,7 @@ import { ArticleSubDetail } from "./ArticleSubDetail";
 export const ArticleDetail = (props: DetailAnimeObj | undefined) => {
   return (
     <section className=" flex flex-col w-full gap-6">
-      <div className="flex flex-col items-center gap-4 h-auto  sm:flex-col-reverse md:flex-row-reverse md:items-start xl:gap-10">
+      <div className="flex flex-col-reverse items-center gap-4 h-auto  md:flex-row-reverse md:items-start xl:gap-10">
         <ArticleDetailCards {...props} />
         <ArticleSubDetail {...props} />
       </div>
--- src/components/ArticleSubDetail.tsx
+++ src/components/ArticleSubDetail.tsx
@@ -1,32 +1,60 @@
+import { useState } from "react";
 import { DetailAnimeObj } from "../types";
 import { Achievements } from "./Achievements";
 import { ChannelSocial } from "./ChannelSocial";
 import { Description } from "./Description";
 import { DetailGenre } from "./DetailGenre";
-// import PlayerYt from "./PlayerYt";
+import PlayerYt from "./PlayerYt";
 import Recommendation from "./Recommendation";
 
+type Player = "youtube" | "anime";
+
 export const ArticleSubDetail = (props: DetailAnimeObj) => {
+  const [playerState, setPlayerState] = useState<Player>("youtube");
+
   const {
     description,
     recommendations,
     genres,
     studios,
-    // trailer,
-    // title,
+    trailer,
+    title,
     characters,
   } = props;
 
   return (
     <article className=" w-full flex flex-col gap-8 h-full md:w-[60%] lg:w-[70%]">
       <article className="  w-full rounded-md overflow-hidden cursor-pointer  aspect-video">
-        {/* <PlayerYt
-          children=""
-          src={trailer?.id}
-          poster={trailer?.thumbnail}
-          title={title?.english ? title.english : ""}
-        /> */}
+        {playerState === "youtube" ? (
+          <PlayerYt
+            children=""
+            src={trailer?.id}
+            poster={trailer?.thumbnail}
+            title={title?.english ? title.english : ""}
+          />
+        ) : (
+          ""
+        )}
       </article>
+
+      <div className=" w-full rounded-sm relative flex flex-col items-center justify-end gap-1">
+        <h1 className=" text-white font-semibold">
+          Free to Watch to any Device
+        </h1>
+        <p className=" text-white/80 text-xs font-semibold">
+          To improve this Webisite we accept donation via Gcash
+        </p>
+        <button
+          className=" bg-white text-black flex items-center justify-center rounded-md px-7 py-1 font-bold text-sm mt-2 cursor-pointer"
+          onClick={() => setPlayerState("anime")}
+        >
+          Watch Now
+        </button>
+        {/* <span className=" text-[11px] text-gray-500 font-semibold">
+          Dont forget to subscribe
+        </span> */}
+      </div>
+
       <Recommendation recommend={recommendations} />
       <div className=" grid grid-cols-2">
         <DetailGenre title="Genre" list={genres} />
@@ -38,3 +66,7 @@ export const ArticleSubDetail = (props: DetailAnimeObj) => {
     </article>
   );
 };
+
+export function Watch() {
+  return <div className=" h-full w-full bg-stone-500"></div>;
+}
