f0a005ad92e2e78344ad5c90fa909a81464596cb
Rocky Jay <rockyjay_g@yahoo.com>
2024-09-19 22:41:49 -0700

    adding select option to choose to easily find the episode they like

--- src/components/Series/Episodes.tsx
+++ src/components/Series/Episodes.tsx
@@ -11,7 +11,7 @@ const Episodes = ({
   episodes: EpisodeType[];
 }) => {
   const { changeEpisode, episode } = useContext(ContextEpisode);
-  const { filter } = useEpisodes(episodes!);
+  const { filter, episodesArray, pages, nextPages } = useEpisodes(episodes!);
 
   function strFunction(strEp: string): string {
     const str = strEp.split(" ").slice(1).join();
@@ -19,8 +19,37 @@ const Episodes = ({
   }
 
   return (
-    <div className={` w-full flex flex-col gap-4 ${className}`}>
-      <ul className=" flex flex-wrap items-center gap-1 justify-center">
+    <article
+      className={` w-full flex flex-col gap-4 overflow-hidden  ${className}`}
+    >
+      <form className=" w-full px-1 overflow-hidden pb-2">
+        <label className="block mb-2 text-lg font-medium text-white">
+          Episodes
+        </label>
+        <select
+          id="countries"
+          className=" bg-neutral-800 w-full text-white text-sm font-semibold rounded-lg p-2.5 min-h-[2rem]"
+          onChange={(e) => nextPages(Number(e.target.value))}
+        >
+          {episodesArray.map((item) => (
+            <option
+              key={item?.id}
+              defaultValue={pages}
+              value={item?.id}
+              className=" mt-1"
+            >
+              {item?.pagePrev} - {item?.pageNext}
+            </option>
+          ))}
+          {/* <option selected>View Episodes</option>
+          <option value="US">United States</option>
+          <option value="CA">Canada</option>
+          <option value="FR">France</option>
+          <option value="DE">Germany</option> */}
+        </select>
+      </form>
+
+      <ul className=" grid grid-cols-6 gap-1 content-center mt-2 md:mt-2 md:grid-cols-5">
         {filter[0]?.episodesAnime?.map((item) => (
           <li
             key={item.id}
@@ -28,7 +57,7 @@ const Episodes = ({
               item.id === episode
                 ? "bg-stone-400 text-black"
                 : "bg-stone-700/85 text-stone-300"
-            }  h-[3rem] w-[3rem] shadow-md cursor-pointer flex items-center justify-center flex-col flex-wrap  font-semibold `}
+            }  h-[3rem]  shadow-md cursor-pointer flex items-center justify-center flex-col flex-wrap  font-semibold `}
             onClick={() => changeEpisode(item.id)}
           >
             <h1 className=" text-sm">EP</h1>
@@ -36,7 +65,7 @@ const Episodes = ({
           </li>
         ))}
       </ul>
-    </div>
+    </article>
   );
 };
 
