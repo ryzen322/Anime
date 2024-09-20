54f845ed009e67c42005146de1e6a64dd7fce0d7
Rocky Jay <rockyjay_g@yahoo.com>
2024-09-20 06:06:23 -0700

    desinging the collection page

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,12 +1,40 @@
 import { useParams } from "react-router-dom";
 import Layout from "./Layout";
 
+const fakeArray = Array.from({ length: 20 }, (_, index) => index + 1);
+
 const Collection = () => {
   const { title } = useParams();
-  console.log(title);
+  title;
+
   return (
     <Layout>
-      <div className=" text-white">{title}</div>
+      <section className=" w-full h-[72.3dvh] relative">
+        <ul className=" grid grid-cols-3 gap-3 w-full overflow-y-scroll no-scrollbar h-full">
+          {fakeArray.map((_, index) => (
+            <li
+              key={index}
+              className=" h-[175px] min-h-[175px] flex items-center bg-stone-500 rounded-md overflow-hidden relative"
+            >
+              <img
+                src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg"
+                className=" w-full h-full object-cover absolute top-0 left-0"
+                alt=""
+              />
+              <div
+                className={`relative w-full h-full top-0 left-0 bottom-0  flex flex-col justify-end items-start bg-gradient-to-b from-[#101014]/10 to-[#16161b] p-2`}
+              >
+                <h1 className=" text-stone-300 text-xs font-semibold">
+                  Attack On Titan
+                </h1>
+                <div className=" flex items-start flex-wrap clear-start text-[11px] font-medium text-stone-400">
+                  <p>Action</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 };
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -49,6 +49,9 @@ const Player = (props: MediaPlayerProps) => {
       crossOrigin
       playsInline
       onProviderChange={onProviderChange}
+      onEnded={(e) => {
+        console.log(e);
+      }}
       ref={player}
     >
       <MediaProvider>
