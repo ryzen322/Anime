da06cce8faa97854c09359b57b9a1926e49d941b
Rocky Jay <rockyjay_g@yahoo.com>
2024-10-01 02:42:19 -0700

    design the header of news

--- src/page/NewsPage.tsx
+++ src/page/NewsPage.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getNews } from "../services/api";
-const fakeArray = Array.from({ length: 5 }, (_, index) => index + 1);
+// const fakeArray = Array.from({ length: 5 }, (_, index) => index + 1);
 
 const NewsPage = () => {
   const { data } = useQuery({
@@ -13,15 +13,40 @@ const NewsPage = () => {
   return (
     <main className=" container mx-auto mt-[4.5rem] ">
       <section className=" h-[82dvh] w-full flex flex-col ">
-        <div className=" bg-stone-400 overflow-hidden w-full h-[12rem] relative">
-          <img
-            className=" w-full h-full object-cover relative"
-            src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/173694-zkZHpXUKouoC.jpg"
-            alt=""
-          />
-          <div className=" absolute top-0 left-0 bg-gradient-to-b from-[#101014]/10 to-[#101014] z-20 h-full w-full"></div>
+        <div className=" bg-stone-400 overflow-hidden w-full h-[32rem] relative sm:rounded-t-md md:h-[25rem] md:rounded-none">
+          <picture>
+            <source
+              media="(max-width: 768px)"
+              className=" rounded-sm"
+              srcSet="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg"
+            />
+            <img
+              loading="lazy"
+              decoding="async"
+              className=" w-full h-full object-cover relative rounded-sm"
+              src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/21459-yeVkolGKdGUV.jpg"
+              alt=""
+            />
+          </picture>
+          <div className=" absolute top-0 left-0 bg-gradient-to-b from-[#101014]/10 to-[#101014] z-20 h-full w-full flex flex-col items-center justify-center gap-2 text-center phoneX:justify-end phoneX:pb-[6rem] md:items-start md:pl-6">
+            <h1 className=" text-stone-300 text-2xl font-bold phoneX:text-4xl">
+              Chainsaw Man
+            </h1>
+            <p className=" text-stone-300 font-medium text-sm max-w-[17rem] leading-4 phoneX:text-base phoneX:leading-[18px] phoneX:max-w-[19rem]">
+              Denji has simple dream - to live a happy and paceful life,
+              spending time with a girl
+            </p>
+            <div className=" flex items-center justify-center gap-2 h-[2.5rem]">
+              <button className=" bg-white h-full text-sm font-bold px-3 rounded-md cursor-pointer">
+                Learn More
+              </button>
+              <button className=" bg-[#292929] h-full text-sm font-bold px-3 rounded-md cursor-pointer text-white">
+                To Watch
+              </button>
+            </div>
+          </div>
         </div>
-        <div className=" grid grid-cols-5 w-full h-[7rem] relative -top-[2.75rem] z-20 gap-2 px-2 phoneX:h-[9rem] sm:gap-2 sm:h-[11rem]">
+        {/* <div className=" grid grid-cols-5 w-full h-[7rem] relative -top-[1.75rem] z-20 gap-2 px-2 phoneX:h-[9rem] sm:gap-2 sm:h-[11rem]">
           {fakeArray.map((_, index) => (
             <div
               key={index}
@@ -35,7 +60,7 @@ const NewsPage = () => {
               <div className=" absolute top-0 left-0 bg-gradient-to-b from-[#101014]/10 to-[#101014] z-20 h-full w-full" />
             </div>
           ))}
-        </div>
+        </div> */}
       </section>
     </main>
   );
