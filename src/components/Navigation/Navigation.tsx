8d76fb717ea46e5bf02bf78665f24b1056685475
Rocky Jay <rockyjay_g@yahoo.com>
2024-11-25 03:59:19 -0800

    fixing the navigation css

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,11 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
 import NavigationMenu from "./NavigationMenu";
-
+import { FiLogIn } from "react-icons/fi";
 import SearchModal from "../modal/SearchModal";
 import { togglePortal } from "../../utils/togglePortal";
 import { Link } from "react-router-dom";
-import { useUser } from "@clerk/clerk-react";
+import { SignInButton, useUser } from "@clerk/clerk-react";
 import Image from "../common/search/Image";
 
 const Navigation = () => {
@@ -42,6 +42,8 @@ const Navigation = () => {
     togglePortal();
   }
 
+  console.log(user);
+
   return (
     <>
       <header
@@ -51,14 +53,20 @@ const Navigation = () => {
         <nav className=" container mx-auto flex items-center gap-2 relative z-50 w-full h-full  lg:gap-5">
           <div className=" flex items-center gap-1">
             <div
-              className=" h-[35px] w-[35px] rounded-full bg-stone-700 cursor-pointer"
+              className=" h-[35px] w-[35px] rounded-full bg-stone-700 cursor-pointer flex items-center justify-center"
               onClick={(): void => setCheState(!chevState)}
             >
-              {user && (
+              {user ? (
                 <Image
                   src={user?.imageUrl}
                   className=" w-full h-full rounded-full object-contain"
                 />
+              ) : (
+                <SignInButton>
+                  <button className=" w-full h-full rounded-full flex items-center justify-center pr-1">
+                    <FiLogIn className=" text-lg text-stone-300 font-bold" />
+                  </button>
+                </SignInButton>
               )}
             </div>
             <IoChevronDownOutline
@@ -72,33 +80,41 @@ const Navigation = () => {
             to={"/"}
             className={`text-xl font-semibold uppercase relative ${
               chevState ? " opacity-0" : " opacity-100"
-            } transition-all  duration-200`}
+            } transition-all  duration-200 lg:opacity-100`}
           >
             My-Anime
           </Link>
 
           <Link
             to={`collection/${"Trending"}`}
-            className=" hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1"
+            className={` hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1 ${
+              chevState ? " opacity-0" : " opacity-100"
+            } transition-all  duration-200 lg:opacity-100`}
           >
             Collection
           </Link>
 
           <Link
             to={"/"}
-            className=" hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1"
+            className={` hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1 ${
+              chevState ? " opacity-0" : " opacity-100"
+            } transition-all  duration-200 lg:opacity-100`}
           >
             Store
           </Link>
           <Link
             to={"/news"}
-            className=" hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1"
+            className={` hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1 ${
+              chevState ? " opacity-0" : " opacity-100"
+            } transition-all  duration-200 lg:opacity-100`}
           >
             News
           </Link>
           <Link
             to={"/dashboard/favorites"}
-            className=" hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1"
+            className={` hidden md:flex  text-base font-semibold text-stone-300 hover:underline mt-1 ${
+              chevState ? " opacity-0" : " opacity-100"
+            } transition-all  duration-200 lg:opacity-100`}
           >
             Dashboard
           </Link>
